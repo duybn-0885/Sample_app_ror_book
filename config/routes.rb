ac93a74db99a13005fe63fab48504beec20f3c0c
duybn-0885 <nd26041998@gmail.com>
2019-04-18 18:11:01 +0700

    chapter 8 v5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root "static_page#home"
 
   get "/help", to: "static_page#help"
@@ -6,6 +7,9 @@ Rails.application.routes.draw do
   get "/contact", to: "static_page#contact"
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
   resources :users
 end
